70a6bb4ecf7b35da1d492d33af3f7a19f1c46b1a
UserPietro <pietro.puozzo@gmail.com>
2025-04-28 12:11:35 +0200

    prova commit

--- src/main.jsx
+++ src/main.jsx
@@ -2,23 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './service-worker.js'
-import App from './App.jsx'
-import { useState } from 'react'
-import TestAPI from './testAPI.jsx'
-import TabellaMeteo from './TabellaMeteo.jsx'
-import ProvaComponet from './ProvaComponet.jsx'
+import './App.css'
 import Input2 from './input.jsx'
 
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* <App /> */}
-    {/* {console.log(getWeather("padova"))} */}
-    {/* <TestAPI city={"Padova"}></TestAPI> */}
-    {/* <ProvaComponet /> */}
-    {/* <TabellaMeteo city={"Padova"}></TabellaMeteo> */}
-    {/* <InputConBottone/> */}
     <Input2></Input2>
     <script src="./service-worker.js"></script>
   </StrictMode>,
