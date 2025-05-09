783500f1fd076d3c782d071061cc34690826ca07
Pierfo <pierfouliana2003@gmail.com>
2025-05-09 10:33:16 +0200

    Updates

---
+++ service-worker.js
@@ -0,0 +1,9 @@
+console.log("ciao")
+
+self.addEventListener("install", () => {
+    console.log("Service worker installed");
+});
+
+self.addEventListener("activate", () => {
+    console.log("service worker activated");
+})
