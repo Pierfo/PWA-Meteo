fe27fa163cfe0841118c0e5e7c46c0f87cb3a58e
UserPietro <pietro.puozzo@gmail.com>
2025-05-14 23:28:21 +0200

    sistemazione altezza skeleton Changes to be committed: 	modified:   dist/index.html 	modified:   src/MeteoCard.jsx

--- src/MeteoCard.jsx
+++ src/MeteoCard.jsx
@@ -266,7 +266,7 @@ function MeteoCard({city}){
       {/* schede oscurate mente si carica l'API */}
       {letturaAPI ? (
         f.map((g, i) =>(        
-          <Card key={g} sx={{ minWidth: 350, margin: "auto", mt: 4 }}>
+          <Card key={g} sx={{ minWidth: 350, height: 170, margin: "auto", mt: 4 }}>
             <CardContent sx={{ display: "flex", justifyContent: "space-between", alignItems: "flex-start" }}>
               <Box>
                 <Typography variant="h5">{getGiornoDellaSettimana(datiMeteo.hourly.time[i*24+1 +1])}</Typography>
@@ -300,8 +300,8 @@ function MeteoCard({city}){
           <Skeleton
             key={i}
             variant="rectangular"
-            width={300}
-            height={200}
+            width={350}
+            height={170}
             animation="wave"
             sx={{ minWidth: 350, margin: "auto", mt: 4 }}
           />
