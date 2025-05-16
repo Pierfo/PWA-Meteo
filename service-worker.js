fffd1b6bf91adf3eca165683e4822a4744c2376f
Pierfo <pierfouliana2003@gmail.com>
2025-05-16 10:01:58 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,10 +1,39 @@
-console.log("ciao")
+const cacheName = "PWA-Meteo_v1"
 
-self.addEventListener("install", () => {
-    console.log("Service worker installed");
+self.addEventListener("install", (e) => {
+    e.waitUntil(() => caches.open(cacheName));
 });
 
-self.addEventListener("activate", () => {
-    clients.claim();
-    console.log("service worker activated");
+self.addEventListener("activate", (e) => {
+    e.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map(cache => {
+                    if(cache != cacheName) {
+                        return caches.delete(cache);
+                    }
+                })
+            )
+        })
+    )
+})
+
+self.addEventListener("fetch", (e) => {    
+    e.respondWith(
+        fetch(e.request).then((res) => {
+            let original_date;
+            for(const pair of res.headers) {
+                if(pair[0] === "Date") {
+                    original_date = Date.parse(pair[1])
+                    console.log("original_date");
+                }
+            }
+            
+            const resClone = res.clone();
+
+            caches.open(cacheName).then((cache) => {cache.put(e.request, resClone)});
+
+            return res;
+        })
+    )
 })
