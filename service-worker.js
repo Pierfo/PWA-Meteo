f12e1e8f07d1749ca222e331696bd3c6fbf00433
Pierfo <pierfouliana2003@gmail.com>
2025-05-17 17:45:28 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -28,7 +28,7 @@ self.addEventListener("fetch", (e) => {
                         const time_cached = parseInt(res.statusText);
 
                         if(Date.now() - time_cached > expirationMinutes * 60 * 1000) {
-                            console.log("TOO OLD!");
+                            resolve(fetchFromWebWrapper(e.request));
                         }
                     }
                 })
