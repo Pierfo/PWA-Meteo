51c0c6014910bde42ca02300ee11780f9ecb7917
Pierfo <pierfouliana2003@gmail.com>
2025-05-17 09:31:35 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -53,6 +53,7 @@ function fetchFromWeb(request) {
             
             if(request.url.includes("https://api.open-meteo.com/v1/forecast")) {
                 caches.open(cacheNames[1]).then((cache) => {cache.put(request, new Response(Date.now().toString()))});
+                caches.open(cacheNames[1]).then((cache) => {cache.match(request).then((res) => console.log(res.body))})
             }
 
             resolve(resClone);
