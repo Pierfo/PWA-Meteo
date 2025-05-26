297746b52f8897450965a62e53dd5b370365f80d
Pierfo <pierfouliana2003@gmail.com>
2025-05-26 17:00:11 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const cacheNames = ["PWA-Meteo_v21", "PWA-Meteo_time-cached_v19"];
+const cacheNames = ["PWA-Meteo_v22", "PWA-Meteo_time-cached_v20"];
 const expirationMinutes = 60;
 
 //C'è forse bisogno di inserire già degli elementi in cache
